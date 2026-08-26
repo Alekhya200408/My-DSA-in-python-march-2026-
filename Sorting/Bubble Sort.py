db964bedb097631fc621ed11ed51168c5fb20c4c
Alekhya200408 <officialalekhya2004@gmail.com>
2026-08-26 20:33:37 +0530

    Refactor bubble sort to improve efficiency by adding early exit condition

--- Sorting/Bubble Sort.py
+++ Sorting/Bubble Sort.py
@@ -3,12 +3,17 @@ def bubble_sort(numbers):
 
 # in loop of range python does not include the stop values
     for i in range(n-1,0,-1):
+        swapped = False
+
         for j in range(i):
             if(numbers[j]>numbers[j+1]):
                 numbers[j],numbers[j+1]=(
                             numbers[j+1],
                             numbers[j]
                         )
+                swapped = True
+        if not swapped:
+            break
     return numbers
 
 values=[9,20,13,26,7,8]
