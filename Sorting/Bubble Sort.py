c3465bf5a9e70782a9b6c4a46934c002c7c2e17d
Alekhya200408 <officialalekhya2004@gmail.com>
2026-08-26 20:18:43 +0530

    Add bubble sort implementation

---
+++ Sorting/Bubble Sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(numbers):
+    n=len(numbers)
+
+# in loop of range python does not include the stop values
+    for i in range(n-1,0,-1):
+        for j in range(i):
+            if(numbers[j]>numbers[j+1]):
+                numbers[j],numbers[j+1]=(
+                            numbers[j+1],
+                            numbers[j]
+                        )
+    return numbers
+
+values=[9,20,13,26,7,8]
+
+print(bubble_sort(values))
+
